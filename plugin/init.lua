1ab0dd22cd8959008eb7e2a6954f8331a75b2778
Raeein <raeeinbagheri80@gmail.com>
2023-04-09 15:03:45 -0400

    take three

--- plugin/init.lua
+++ plugin/init.lua
@@ -6,16 +6,17 @@ end
 vim.g.screenshot_nvim_loaded = 1
 
 -- change this to true if your are developing locally
-local installed_locally = false
+-- local installed_locally = false
 
 
-if installed_locally then
-    vim.cmd("command! SS :lua require('lua.screenshot_nvim.screenshot_nvim').SS()")
-    vim.cmd("command! -range=% -nargs=0 SSText :'<,'>lua require('lua.screenshot_nvim.screenshot_nvim').SSText()")
-else
-    vim.cmd("command! SS :lua require('screenshot_nvim.screenshot_nvim').SS()")
-    vim.cmd("command! -range=% -nargs=0 SSText :'<,'>lua require('screenshot_nvim.screenshot_nvim').SSText()")
-end
+-- if installed_locally then
+--     print("here")
+--     vim.cmd("command! SS :lua require('lua.screenshot_nvim.screenshot_nvim').SS()")
+--     vim.cmd("command! -range=% -nargs=0 SSText :'<,'>lua require('lua.screenshot_nvim.screenshot_nvim').SSText()")
+-- else
+vim.cmd("command! SS :lua require('screenshot_nvim.screenshot_nvim').SS()")
+vim.cmd("command! -range=% -nargs=0 SSText :'<,'>lua require('screenshot_nvim.screenshot_nvim').SSText()")
+-- end
 
 
 vim.g.mapleader = " "
