fd287dd7e5c3c4cf5c1d9f3f710815da1af309bc
Raeein <raeeinbagheri80@gmail.com>
2023-04-09 14:55:45 -0400

    trying to fix require issue with packer

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,12 +1,22 @@
 if vim.g.screenshot_nvim_loaded then
-	print("screenshot_nvim already loaded")
+	-- print("screenshot_nvim already loaded")
 	return
 end
 
 vim.g.screenshot_nvim_loaded = 1
 
-vim.cmd("command! SS :lua require('lua.screenshot_nvim.screenshot_nvim').SS()")
-vim.cmd("command! -range=% -nargs=0 SSText :'<,'>lua require('lua.screenshot_nvim.screenshot_nvim').SSText()")
+-- change this to true if your are developing locally
+local installed_locally = false
+
+
+if installed_locally then
+    vim.cmd("command! SS :lua require('screenshot_nvim.screenshot_nvim').SS()")
+    vim.cmd("command! -range=% -nargs=0 SSText :'<,'>lua require('screenshot_nvim.screenshot_nvim').SSText()")
+else
+    vim.cmd("command! SS :lua require('lua.screenshot_nvim.screenshot_nvim').SS()")
+    vim.cmd("command! -range=% -nargs=0 SSText :'<,'>lua require('lua.screenshot_nvim.screenshot_nvim').SSText()")
+end
+
 
 vim.g.mapleader = " "
 vim.keymap.set({"v"}, "<leader>ss", ":lua SS()<CR>", {noremap = true, silent = true})
